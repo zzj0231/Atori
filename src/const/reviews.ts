dd5bb446fd250be8a0c1926228ce6bc1d53cb89c
ZZJsky123 <1647372420@qq.com>
2024-10-28 23:23:26 +0800

    pref: add two reviews

--- src/const/reviews.ts
+++ src/const/reviews.ts
@@ -1,4 +1,5 @@
 import frierenUrl from '../../public/diary/frieren.png'
+import humanBondage from '../../public/diary/human-bondage.jpg'
 
 export enum RE_LABEL {
   'notion' = '小说',
@@ -9,6 +10,13 @@ export enum RE_LABEL {
   'science' = '科学',
   'movie' = '电影',
   'animate' = '动漫',
+  'grow' = '成长',
+  'life' = '生活',
+  'soul' = '生命',
+  'love' = '爱情',
+  'fiction' = '科幻',
+  'fantasy' = '奇幻',
+  'funny' = '搞笑',
 }
 
 export const RE_THEME_TYPE = [
@@ -23,16 +31,25 @@ export const RE_THEME_TYPE = [
 ]
 
 export const RE_CUSTOME_TYPE = [
-  { key: 'grow', label: '成长' },
-  { key: 'life', label: '生活' },
-  { key: 'soul', label: '生命' },
-  { key: 'love', label: '爱情' },
-  { key: 'fiction', label: '科幻' },
-  { key: 'fantasy', label: '奇幻' },
-  { key: 'funny', label: '搞笑' },
+  { key: 'grow', label: RE_LABEL.grow },
+  { key: 'life', label: RE_LABEL.life },
+  { key: 'soul', label: RE_LABEL.soul },
+  { key: 'love', label: RE_LABEL.love },
+  { key: 'fiction', label: RE_LABEL.fiction },
+  { key: 'fantasy', label: RE_LABEL.fantasy },
+  { key: 'funny', label: RE_LABEL.funny },
 ]
 
 export const RE_BLUM = [
+  {
+    name: '人性的枷锁',
+    author: '英国 / 威廉·萨默塞特·毛姆',
+    cover: humanBondage,
+    note: '',
+    labels: [RE_LABEL.animate],
+    key: 'frieren',
+    stars: 5,
+  },
   {
     name: '葬送的芙莉莲',
     author: '日本 / 山田钟人',
