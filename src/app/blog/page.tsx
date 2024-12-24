d3e186035e60bca7d934d7d207153bf421775736
ZZJsky123 <1647372420@qq.com>
2024-12-25 00:32:30 +0800

    chore: blog 暂时文案

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,15 @@
-export default function Blog() {
-  return <>blog</>
+import { Demo } from '@/server'
+
+export default async function Blog() {
+  return (
+    <>
+      <div className="flex items-center flex-col gap-3">
+        <span className="opacity-50">施工中...</span>
+      </div>
+
+      {/* <div>
+        <Demo />
+      </div> */}
+    </>
+  )
 }
